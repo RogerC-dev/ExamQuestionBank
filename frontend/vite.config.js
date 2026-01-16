46e80f83bdb6e578e6f449c4e9e5d0ed9d084e1d
Leo890728 <48422857+Leo890728@users.noreply.github.com>
2026-01-16 15:18:04 +0800

    feat(vite): enhance configuration to load environment variables dynamically

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    proxy: {
-      // 將 /api 的要求代理到 Django 伺服器
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-      },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.VITE_BASE_PATH || '/',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     },
+    server: {
+      proxy: {
+        // 將 /api 的要求代理到 Django 伺服器
+        '/api': {
+          target: 'http://127.0.0.1:8000',
+          changeOrigin: true,
+        },
+      },
+    }
   }
 })
